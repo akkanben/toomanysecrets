dce1d4119c958a8c5ce6261950a4177f162dd275
Ben Mills <fuzzyllama@gmail.com>
2022-03-03 13:29:04 -0800

    Adds initial spring initializr code

---
+++ src/main/java/com/akkanben/toomanysecrets/ToomanysecretsApplication.java
@@ -0,0 +1,13 @@
+package com.akkanben.toomanysecrets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToomanysecretsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToomanysecretsApplication.class, args);
+	}
+
+}
